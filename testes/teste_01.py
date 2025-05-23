a9e20bdb358a785d4c5e2e481023f5b708f74a1a
Rafael395 <166072605+Rafael395@users.noreply.github.com>
2025-05-22 21:14:57 -0300

    Update teste_01.py

--- testes/teste_01.py
+++ testes/teste_01.py
@@ -1,1 +1,20 @@
+import requests
 
+# Configuração
+url = "https://jsonplaceholder.typicode.com/posts/1"
+
+# Execução
+response = requests.get(url)
+
+# Testes
+if response.status_code == 200:
+    print("✅ Status 200 OK")
+else:
+    print(f"❌ Falha: Status {response.status_code}")
+
+if response.json()["userId"] == 1:
+    print("✅ userId correto (== 1)")
+else:
+    print("❌ userId incorreto")
+
+print("\nResposta completa da API:", response.json())
